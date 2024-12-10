a13410349026583ba6d1180eda753b579b05a076
mariana <00001114975758sp@al.educacao.sp.gov.br>
2024-12-10 17:58:55 -0300

    Update respostas.js

--- respostas.js
+++ respostas.js
@@ -1,8 +1,49 @@
-criaCartao(
-    'TEMA',
-    'PERGUNTA',
-    'RESPOSTA'
-)
+criaCartao('Música Brasileira Contemporânea', 'Qual cantora ficou famosa com o álbum "Anjo do Amor", lançado em 2004?', 'Maria Gadú');
+criaCartao('Música Brasileira', 'Quem foi o autor do hit "Ai Se Eu Te Pego", lançado em 2011?', 'Michel Teló');
+criaCartao('Sertanejo Universitário', 'Qual dupla sertaneja ficou famosa por músicas como "Largado às Traças" e "Coração Machucado"?', 'Zezé Di Camargo & Luciano');
+criaCartao('Música Pop Brasileira', 'Quem lançou o álbum "Seu Mundo Não Cabe No Meu", que contém o hit "Faz Parte"?', 'Cláudia Leitte');
+criaCartao('MPB Atual', 'Quem é o cantor e compositor que ficou famoso com a música "O Mundo é um Moinho", regravada em 2016 por Nando Reis?', 'Cartola');
+criaCartao('Música Brasileira', 'Qual cantora lançou o sucesso "Beleza Rara" em 2000?', 'Daniela Mercury');
+criaCartao('Música Brasileira', 'Qual cantor lançou o álbum "24 Horas de Vida", que mistura MPB com música eletrônica?', 'Caetano Veloso');
+criaCartao('Funk Carioca', 'Quem é o MC responsável pelo hit "Rap do Silva"?', 'MC Silva');
+criaCartao('Música Brasileira', 'Qual cantor sertanejo ficou famoso por músicas como "Deixa a Gente se Encontrar" e "Te Assumi pro Brasil"?', 'Luan Santana');
+criaCartao('MPB', 'Quem é o cantor e compositor da música "O Vento"?', 'Los Hermanos');
+criaCartao('Música Brasileira', 'Qual artista brasileiro ficou famoso com canções como "Aquarela do Brasil", regravadas após 2000?', 'Ary Barroso');
+criaCartao('Música Brasileira', 'Quem ficou famoso por canções como "Pais e Filhos" e "Quando o Sol Bater na Janela do Teu Quarto"?', 'Cássia Eller');
+criaCartao('MPB', 'Qual cantor e compositor brasileiro fez sucesso com "Velha Infância", de 1999?', 'Tribalistas');
+criaCartao('Música Brasileira', 'Quem é o cantor brasileiro que ficou conhecido pelo sucesso "Você Vai Ver"?', 'Kiko Zambianchi');
+criaCartao('Samba', 'Qual cantor brasileiro popularizou a música "O Show Tem Que Continuar"?', 'Zeca Pagodinho');
+criaCartao('Funk Carioca', 'Qual foi o hit de Anitta que levou ao reconhecimento nacional e internacional em 2013?', 'Show das Poderosas');
+criaCartao('Música Brasileira', 'Qual banda brasileira de pop-rock fez sucesso nos anos 2000 com o álbum "Acústico MTV"?', 'Legião Urbana');
+criaCartao('Música Brasileira', 'Quem lançou o álbum "Boca de Lobo", em 2013?', 'Carlinhos Brown');
+criaCartao('Sertanejo Universitário', 'Quem foi a dupla responsável pela música "Te Esperando"?', 'Marcos & Belutti');
+criaCartao('Música Brasileira', 'Qual cantor brasileiro de reggae é conhecido pela música "Festa no Apê"?', 'Ivan Lins');
+criaCartao('Música Brasileira', 'Quem é o cantor de "Trem Bala", que mistura MPB com música pop?', 'Ana Vilela');
+criaCartao('Música Brasileira', 'Quem lançou o hit "Me Adora" em 2012?', 'Pitty');
+criaCartao('Música Brasileira', 'Qual artista brasileira lançou o álbum "Sambachic" em 2014?', 'Preta Gil');
+criaCartao('Pop Brasileiro', 'Quem foi o autor da música "Te Regalo", que ficou famosa nas rádios de 2015?', 'Ivete Sangalo');
+criaCartao('Música Brasileira', 'Quem foi o autor da música "Chorando Se Foi"?', 'Falamansa');
+criaCartao('Música Brasileira', 'Qual cantora ficou famosa por músicas como "O Mundo é Um Moinho"?', 'Maria Gadú');
+criaCartao('Samba', 'Qual cantor brasileiro ficou famoso por músicas como "Festa" e "Samba de Verão"?', 'Jorge Aragão');
+criaCartao('Funk Carioca', 'Qual foi o grande sucesso de MC Kevinho que dominou as paradas em 2017?', 'O Grave Bater');
+criaCartao('Música Brasileira', 'Quem é o cantor que ficou famoso com o hit "Sou Eu"?', 'Tim Maia');
+criaCartao('Música Brasileira', 'Qual artista brasileira ficou famosa por músicas como "Ai Se Eu Te Pego"?', 'Michel Teló');
+criaCartao('Pop/Rock', 'Quem é o cantor brasileiro que ficou famoso com o álbum "Vivo", lançado em 2009?', 'Nando Reis');
+criaCartao('Música Brasileira', 'Qual cantor e compositor lançou o álbum "Cores e Valores"?', 'Emicida');
+criaCartao('MPB', 'Quem é o cantor e compositor brasileiro responsável por "Completamente Incomum"?', 'Fábio Jr.');
+criaCartao('Pop Brasileiro', 'Quem é a cantora responsável por "Vem Dançar Comigo", lançada em 2010?', 'Anitta');
+criaCartao('Sertanejo Universitário', 'Qual dupla fez sucesso com a música "Ai Se Eu Te Pego"?', 'Michel Teló');
+criaCartao('Música Brasileira', 'Qual cantor de reggae ficou famoso com o hit "Me Deixe"?', 'Raça Negra');
+criaCartao('MPB', 'Qual cantor lançou o álbum "Sonho de Davi" em 2004?', 'Davi Moraes');
+criaCartao('Música Brasileira', 'Quem é o cantor de MPB que lançou o sucesso "Tô De Pé"?', 'Zeca Baleiro');
+criaCartao('Música Brasileira', 'Qual artista brasileira ficou famosa por "Evidências"?', 'Chitãozinho & Xororó');
+criaCartao('Sertanejo Universitário', 'Qual dupla sertaneja ficou famosa por "Deixa a Gente Se Encontrar"?', 'Jorge & Mateus');
+criaCartao('Pop/Rock', 'Quem é o cantor brasileiro que fez sucesso com "Alma Gêmea" em 2010?', 'Roupa Nova');
+criaCartao('Música Brasileira', 'Quem é o cantor e compositor responsável pela música "O Sol"?', 'Jorge & Mateus');
+criaCartao('Pop/Rock', 'Quem é o cantor que ficou famoso com a música "Fui Fiel"?', 'Neymar Jr.');
+criaCartao('Samba', 'Qual cantor ficou famoso por músicas como "Na Rua, Na Chuva, Na Fazenda"?', 'Gilberto Gil');
+criaCartao('Música Brasileira', 'Quem é o cantor de samba e MPB que fez sucesso com "Coração Vagabundo"?', 'Caetano Veloso');
+criaCartao('Música Brasileira', 'Qual cantora ficou famosa por seu álbum "Toda Forma de Amor", lançado em 2007?', 'Ivete Sangalo');
 
 
 
